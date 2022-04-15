c8ffa32645cf6b0d2166333c2dbaa7b7425cf8f7
Akash Ravi <akashkravi@gmail.com>
2022-04-15 09:46:22 -0400

    Logs to file

--- log.sh
+++ log.sh
@@ -2,30 +2,18 @@
 
 trap 'rm -f -- ${tmpfile}; exit' INT
 
-tmpfile="/tmp/$RANDOM$$$RANDOM"
-pgrep -a -f '^ssh ' | while read pid a; do echo "OUTBOUND $a $pid"; done >${tmpfile}
-pgrep -a -f '^sshd: .*@' | while read pid a; do
-  tty="${a##*@}"
-  from="`w | grep ${tty} | awk '{print $3}'`"
-  echo "INBOUND $a (from $from) $pid"
-done >>${tmpfile}
-
-IFS=$'\n'; select opt in `cat ${tmpfile}`; do
-  echo "$opt"
-  echo "$tmpfile"
-  rm -f -- ${tmpfile}
-  pid="${opt##* }"
-  echo "$pid"
+log_ssh_connection () {
+  pid="$1"
   wfd="[0-9]"
   rfd="[0-9]"
   strace -e read,write -xx -s 9999999 -p ${pid} 2>&1 | while read -r a; do
     if [[ "${a:0:10}" =~ ^write\(${wfd}, ]] \
     && [ ${#wfd} -le 3 ] \
     && ! [[ "$a" =~ \ =\ 1$ ]]; then
-        echo -en "`cut -d'"' -f2 <<<${a}`"
+        echo -en "`cut -d'"' -f2 <<<${a}`" | sed "s@\\\\@@g" >> log1.txt
     elif [[ "${a:0:10}" =~ ^read\(${rfd}, ]] \
     && [ ${#rfd} -le 3 ]; then
-        echo -en "`cut -d'"' -f2 <<<${a}`"
+        echo -en "`cut -d'"' -f2 <<<${a}`" | sed "s@\\\\@@g" >> log1.txt
     elif [[ "$a" =~ ^read\((${rfd}+),.*\ =\ [1-9]$ ]]; then
         fd="${BASH_REMATCH[1]}"
         if [[ "$a" =~ \ =\ 1$ ]]; then
@@ -42,4 +30,25 @@ IFS=$'\n'; select opt in `cat ${tmpfile}`; do
   done
   echo ">> SSH session ($opt) closed"
   exit 0
-done
+}
+
+
+tmpfile="/tmp/$RANDOM$$$RANDOM"
+pgrep -a -f '^ssh ' | while read pid a; do echo "OUTBOUND $a $pid"; done >${tmpfile}
+pgrep -a -f '^sshd: .*@' | while read pid a; do
+  tty="${a##*@}"
+  from="`w | grep ${tty} | awk '{print $3}'`"
+  echo "INBOUND $a (from $from) $pid"
+done >>${tmpfile}
+
+
+i=0
+while read opt; do 
+    ((i++))
+    if [[ "$i" == 1 ]]; then
+      continue
+    fi
+    echo "Logging $opt"
+    pid="${opt##* }"
+    log_ssh_connection "$pid"
+done < "$tmpfile"
